6056fbee773b7708e2755fea95ffaeb86e0c62fc
MAbuhanif <mundinoabete@gmail.com>
2024-03-05 22:36:41 +0000

    file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import './App.css'
-import About from './components/About/About'
-import Contact from './components/Contact/Contact'
-import Home from './components/Home/Home'
-import NavBar from './components/Nav/NavBar'
-import Portifolio from './components/Portifolio/Portifolio'
-import Skills from './components/Skills/Skills'
-import SocialLinks from './components/SocialLinks/SocialLinks'
-
+import "./App.css";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Home from "./components/Home/Home";
+import NavBar from "./components/Nav/NavBar";
+import Portifolio from "./components/Portifolio/Portifolio";
+import Skills from "./components/Skills/Skills";
+import SocialLinks from "./components/SocialLinks/SocialLinks";
 
 function App() {
-  
-
   return (
     <>
-     <NavBar/>
-     <Home/>
-     <About/>
-     <Portifolio/>
-     <Skills/>
-     <Contact/>
-     <SocialLinks/>
+      <NavBar />
+      <Home />
+      <About />
+      <Portifolio />
+      <Skills />
+      <Contact />
+      <SocialLinks />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
