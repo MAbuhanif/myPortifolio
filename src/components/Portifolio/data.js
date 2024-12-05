961f35f585413d361da8c2f4b0855b84e3f450bb
MAbuhanif <mundinoabete@gmail.com>
2024-12-05 12:23:24 +0000

    Add flask task manager project

--- src/components/Portifolio/data.js
+++ src/components/Portifolio/data.js
@@ -4,40 +4,47 @@ import taskMgr_terminal from "../../assets/projectsImg/taskMgrTerminal.png";
 import loveSandwich from "../../assets/projectsImg/loveSandwiches.png";
 import loveRunning from "../../assets/projectsImg/loveRunning.png";
 import thorinCompany from "../../assets/projectsImg/flaskThorin.png";
+import flaskTaskMgr from "../../assets/projectsImg/flaskTaskMgr.png";
 
 export const data = [
   {
     id: 1,
+    image: flaskTaskMgr,
+    github: "https://github.com/MAbuhanif/flask-task-mgr.git",
+    live: "https://flask-task-23c5294900c6.herokuapp.com/",
+  },
+  {
+    id: 2,
     image: ethiopianHistory,
     github: "https://github.com/MAbuhanif/ethiopian-history.git",
     live: "https://mabuhanif.github.io/ethiopian-history/",
   },
   {
-    id: 2,
+    id: 3,
     image: task_manager,
     github: "https://mabuhanif.github.io/toDo-list/",
     live: "https://mabuhanif.github.io/toDo-list/",
   },
   {
-    id: 3,
+    id: 4,
     image: taskMgr_terminal,
     github: "https://github.com/MAbuhanif/taskManager.git",
     live: "https://taskm-388f9bd7b7c7.herokuapp.com/",
   },
   {
-    id: 4,
+    id: 5,
     image: loveSandwich,
     github: "https://github.com/MAbuhanif/loveSandwiches.git",
     live: "https://sandwiches-terminal-9ee30e53d4b6.herokuapp.com/",
   },
   {
-    id: 5,
+    id: 6,
     image: loveRunning,
     github: "https://github.com/MAbuhanif/loveRunning.git",
     live: "https://mabuhanif.github.io/loveRunning/",
   },
   {
-    id: 6,
+    id: 7,
     image: thorinCompany,
     github: "https://github.com/MAbuhanif/flask-thorin_and_company.git",
     live: "https://flaskapp-mun-6dde29594883.herokuapp.com/",
