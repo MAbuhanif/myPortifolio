3b6e4ecd37521fb6dd8d098e585622e844fdb9af
MAbuhanif <mundinoabete@gmail.com>
2024-12-05 12:56:12 +0000

    Edit error on data.js

--- src/components/Portifolio/data.js
+++ src/components/Portifolio/data.js
@@ -25,19 +25,19 @@ export const data = [
     live: "https://mabuhanif.github.io/toDo-list/",
   },
   {
-    id: 5,
+    id: 4,
     image: loveSandwich,
     github: "https://github.com/MAbuhanif/loveSandwiches.git",
     live: "https://sandwiches-terminal-9ee30e53d4b6.herokuapp.com/",
   },
   {
-    id: 6,
+    id: 5,
     image: loveRunning,
     github: "https://github.com/MAbuhanif/loveRunning.git",
     live: "https://mabuhanif.github.io/loveRunning/",
   },
   {
-    id: 7,
+    id: 6,
     image: thorinCompany,
     github: "https://github.com/MAbuhanif/flask-thorin_and_company.git",
     live: "https://flaskapp-mun-6dde29594883.herokuapp.com/",
